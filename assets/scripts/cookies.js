c4f841bfbccd378d07d4713ee4dd1ad052289334
Lateefat Obisesan <temilele06@gmail.com>
2026-04-14 22:36:33 -0500

    JS content

--- assets/scripts/cookies.js
+++ assets/scripts/cookies.js
@@ -8,4 +8,17 @@ const saveBtn =document.getElementById('save-settings');
 
 function setCookie(name, value, seconds = 20) {
     document.cookie = `${name}=${value}; max-age=${seconds}; path=/`;
-}
+};
+
+const getCookie = (name) => {
+    const cookies = document.cookie.split(';');
+
+    for (let cookie of cookies) {
+        cookie = cookie.trim();
+        if (cookie.startsWith(name + '=')) {
+            return cookie.substring(name.length + 1);
+        }
+    }
+
+    return null;
+};
