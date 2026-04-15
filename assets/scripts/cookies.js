3ffd58afd0aeba7fcaba2af750f1f9f3163d9497
Lateefat Obisesan <temilele06@gmail.com>
2026-04-14 22:31:42 -0500

    started the JS

--- assets/scripts/cookies.js
+++ assets/scripts/cookies.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const askDialog = document.getElementById('ask-dialog');
+const settingsDialog = document.getElementById('settings-dialog');
+const acceptBtn = document.getElementById('accept');
+const settingsBtn = document.getElementById('settings');
+const saveBtn =document.getElementById('save-settings');
+
+function setCookie(name, value, seconds = 20) {
+    document.cookie = `${name}=${value}; max-age=${seconds}; path=/`;
+}
