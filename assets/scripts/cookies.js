ebf103e03cf6d819822598e95a1a7b84aa94d4ce
Lateefat Obisesan <temilele06@gmail.com>
2026-04-15 06:49:51 -0500

    adding the set and get of cookie

--- assets/scripts/cookies.js
+++ assets/scripts/cookies.js
@@ -6,19 +6,21 @@ const acceptBtn = document.getElementById('accept');
 const settingsBtn = document.getElementById('settings');
 const saveBtn =document.getElementById('save-settings');
 
+const LIFETIME = 20;
+
 function setCookie(name, value, seconds = 20) {
     document.cookie = `${name}=${value}; max-age=${seconds}; path=/`;
 };
 
 const getCookie = (name) => {
+    if (!document.cookie) return null;
     const cookies = document.cookie.split(';');
-
     for (let cookie of cookies) {
-        cookie = cookie.trim();
-        if (cookie.startsWith(name + '=')) {
-            return cookie.substring(name.length + 1);
+        const trimmedCookie = cookie.trim();
+        const [key, value] = trimmedCookie.split('=');
+        if (decodeURIComponent(key) === name) {
+                return decodeURIComponent(value);
         }
     }
-
     return null;
 };
